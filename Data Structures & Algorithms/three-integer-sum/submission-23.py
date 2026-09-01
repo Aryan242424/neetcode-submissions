03d15db7fade7f0c2c204d8ae252c168bf25e6ec
Aryan242424 <aryan.puri@mail.utoronto.ca>
2026-09-01 16:33:36 -0400

    Add: three-integer-sum - submission-23

---
+++ Data Structures & Algorithms/three-integer-sum/submission-23.py
@@ -0,0 +1,49 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        #naive solution attempt
+        res = []
+        nums.sort() # nlogn time
+
+        for i, v in enumerate(nums):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            target = -v
+            sublist = nums[i + 1:]
+            if len(sublist) < 2: continue
+
+            two_sums = self.twoSum(sublist, target) 
+            if not two_sums: continue # skip
+
+            triplets = [i + [v] for i in two_sums]
+
+            res.extend(triplets)
+    
+        return res
+
+
+    def twoSum(self, nums: List[int], target: int) -> List[List[int]]:
+        l = 0
+        r = len(nums) - 1
+        res = []
+    
+        
+        while l < r: # sorted lst btw
+            sum = nums[l] + nums[r]
+            if (sum == target): 
+                res.append([nums[l], nums[r]])
+                l += 1
+                r -= 1
+                while l < r and nums[l] == nums[l - 1] : l +=1
+                while l < r and nums[r] == nums[r + 1] : r -=1
+            elif (sum > target): r -=1
+            else: l +=1
+        return res
+
+
+        
+
+
+
+
+        
